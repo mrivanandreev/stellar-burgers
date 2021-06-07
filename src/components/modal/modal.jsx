a44e8c93c68c767f0cd0fef59e0ec1acdbf52ebb
Ivan Andreev <mr.ivan.andreev@gmail.com>
2021-06-07 16:06:58 +0300

    Fix closing modal by esc press

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import modalStyle from "./modal.module.css";
 import { ModalOverlay } from "../modal-overlay/modal-overlay";
 
@@ -11,9 +11,22 @@ export const Modal = (props) => {
   const { isOpen, setOpen, children } = props;
 
   const closeModal = (ev) => {
-    ev.stopPropagation();;
+    ev.stopPropagation();
     setOpen(false);
-  }
+  };
+
+  useEffect(() => {
+    const handleEscPress = (ev) => {
+      if (ev.key === "Escape")  {
+        closeModal(ev);
+      };
+    }
+
+    document.addEventListener("keydown", handleEscPress);
+    
+    return () => document.removeEventListener("keydown", handleEscPress);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (!isOpen) return null;
 
@@ -21,7 +34,10 @@ export const Modal = (props) => {
     <>
       <ModalOverlay onClick={closeModal} />
       <section className={modalStyle.window}>
-        <button className={`mt-15 mr-10 ${modalStyle.closeButton}`} onClick={closeModal}>
+        <button
+          className={`mt-15 mr-10 ${modalStyle.closeButton}`}
+          onClick={closeModal}
+        >
           <CloseIcon type="primary" />
         </button>
         {children}
@@ -35,5 +51,5 @@ export const Modal = (props) => {
 Modal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   setOpen: PropTypes.func.isRequired,
-  children: PropTypes.element
-}
+  children: PropTypes.element,
+};
