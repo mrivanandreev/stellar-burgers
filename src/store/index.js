3907708b7f92af852587fb813b9f5785af64c6f5
Ivan Andreev <mr.ivan.andreev@gmail.com>
2021-08-10 22:03:25 +0300

    Create store

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers();
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
